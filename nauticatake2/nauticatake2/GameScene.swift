ea3d1a3d947a4bbd06880200d9e1fac346108a59
Simon Chervenak <sarras305@gmail.com>
2019-12-19 13:12:52 -0500

    this sucks

--- nauticatake2/nauticatake2/GameScene.swift
+++ nauticatake2/nauticatake2/GameScene.swift
@@ -9,14 +9,14 @@
 import SpriteKit
 import GameplayKit
 
-let SIZE = 0.1
+let SIZE = 64.0
 let FISH = 72
 
-func make_node(from: Int, size: Double, radius: CGFloat = 0.055) -> SKSpriteNode {
+func make_node(from: Int, size: Double) -> SKSpriteNode {
     let f = String(format: "fishTile_%03d", from)
     let n = SKSpriteNode(imageNamed: f)
     n.size = CGSize(width: size, height: size)
-    n.physicsBody = SKPhysicsBody(circleOfRadius: radius)
+    n.physicsBody = SKPhysicsBody(circleOfRadius: n.texture!.size().width)
     return n
 }
 
@@ -35,15 +35,17 @@ let SPONGES = [
     [98, 52, 53]
 ]
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     private var fish: SKSpriteNode!
     
     private var x = 0
     
     override func didMove(to view: SKView) {
-        self.physicsWorld.gravity = CGVector(dx: -0.1, dy: -0.01)
+        print(self.size)
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         
-//        self.backgroundColor = UIColor(rgb: 0x40d6cc)
+        self.backgroundColor = UIColor(rgb: 0x40d6cc)
         
         
         for x in [3, 7] {
@@ -51,7 +53,7 @@ class GameScene: SKScene {
             for i in sponge {
                 let px = self.size.width * x / 10
                 let sponge_node = make_node(from: i, size: SIZE)
-                sponge_node.position = CGPoint(x: px + Double.random(in: -10...10) / 30, y: 0.5)
+                sponge_node.position = CGPoint(x: px + Double.random(in: -10...10) / 30, y: 500)
 
                 sponge_node.physicsBody?.affectedByGravity = true
                 sponge_node.physicsBody?.allowsRotation = true
@@ -65,15 +67,16 @@ class GameScene: SKScene {
         let ground = [6, 7]
         for x in 0...10 {
             let px = self.size.width * x / 10
+            print("px", px)
             let first = make_node(from: 1, size: SIZE)
-            first.position = CGPoint(x: px, y: 0.15)
+            first.position = CGPoint(x: px, y: 0)
             first.zPosition = 1
 
             first.physicsBody?.affectedByGravity = false
             first.physicsBody?.isDynamic = false
             
             let second = make_node(from: ground[x % ground.count], size: SIZE)
-            second.position = CGPoint(x: px, y: 0.3)
+            second.position = CGPoint(x: px, y: 64)
             second.zPosition = 1
             
             second.physicsBody?.affectedByGravity = false
@@ -84,7 +87,7 @@ class GameScene: SKScene {
         }
         
         self.fish = make_node(from: FISH, size: SIZE)
-        self.fish.position = CGPoint(x: 0.3, y: 0.7)
+        self.fish.position = CGPoint(x: 300, y: 300)
         
         self.fish.physicsBody?.affectedByGravity = true
         self.fish.physicsBody?.velocity.dx = 0
--- nauticatake2/nauticatake2/GameViewController.swift
+++ nauticatake2/nauticatake2/GameViewController.swift
@@ -17,19 +17,19 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            let scene = GameScene()
-            // Set the scale mode to scale to fit the window
-            scene.scaleMode = .aspectFill
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .resizeFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
             view.showsPhysics = true
             
-            // Present the scene
-            view.presentScene(scene)
-                        
-            view.ignoresSiblingOrder = true
-            
-//            view.showsFPS = true
-//            view.showsNodeCount = true
-            
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
